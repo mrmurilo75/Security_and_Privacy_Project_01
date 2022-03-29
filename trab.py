d3756c3fd7de7d5a8d708008655660cac75707f1
thevvalverde <fvalverde007@gmail.com>
2022-03-29 17:14:40 +0100

    bugfix

--- trab.py
+++ trab.py
@@ -1,24 +1,20 @@
 import string
 import random
+import sys
 
-aes = [8, 64, 512, 4096, 32768, 262144, 2047152]
-sha = [8, 64, 512, 4096, 32768, 262144, 2047152]
-rsa = [2, 4, 8, 16, 32, 64, 128]
 
-for size in aes:
-    ran = "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=int(size/4)))
-    f = open("aes" + str(size), "w")
-    f.write(str(ran))
-    f.close()
+algo = {
+    "aes" : [8, 64, 512, 4096, 32768, 262144, 2047152],
+    "sha" : [8, 64, 512, 4096, 32768, 262144, 2047152],
+    "rsa" : [2, 4, 8, 16, 32, 64, 128]
+}
 
-for size in sha:
-    ran = "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=int(size/4)))
-    f = open("sha" + str(size), "w")
-    f.write(str(ran))
-    f.close()
 
-for size in rsa:
-    ran = "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=int(size/4)))
-    f = open("rsa" + str(size), "w")
-    f.write(str(ran))
-    f.close()
+def runCode(algo):
+    for i in range(10):
+        for item in algo:
+            ran = "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=size))
+            f = open(str(algo) + i + "_" + str(size), "w")
+            f.write(str(ran))
+            f.close()
+
