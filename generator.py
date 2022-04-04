fc2419c3acbfd95fccefe1260e9604b16c24e587
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-04 21:31:06 +0100

    update generator.py

--- generator.py
+++ generator.py
@@ -3,7 +3,10 @@ import random
 import os
 
 
-TEST_FILES_DIR = 'test_files'
+_TEST_FILES_DIR = 'test_files'
+TEST_FILES_DIR = _TEST_FILES_DIR if os.path.isdir(_TEST_FILES_DIR) else (os.mkdir(_TEST_FILES_DIR) or _TEST_FILES_DIR)
+
+TEST_LIMIT = 10
 
 algo = {
     "aes" : [8, 64, 512, 4096, 32768, 262144, 2047152],
@@ -11,19 +14,30 @@ algo = {
     "rsa" : [2, 4, 8, 16, 32, 64, 128]
 }
 
+def _generate_string(size): 
+    return "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=size))
+
+def generate_algo(key):
+    generated_files = {}
+
+    for size in algo[key]:
+        generated_files |= { size : [] }
+
+        for i in range(TEST_LIMIT):
+            ran = _generate_string(size)
+            test_file = key + "_t" + str(i) + "_s" + str(size)
+
+            with open( os.path.join(TEST_FILES_DIR, test_file) , "w" ) as ff:
+                ff.write( str(ran) )
+
+            generated_files[size].append(test_file) 
+
+    return generated_files
+
 
 def generate(algo):
-
-    if not os.path.isdir(TEST_FILES_DIR):
-        os.mkdir(TEST_FILES_DIR)
-
-    for i in range(10):
-        for (key, val) in algo.items():
-            for size in val:
-                ran = "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=size))
-                f = open( os.path.join(TEST_FILES_DIR, key + str(i) + "_" + str(size)) , "w" )
-                f.write(str(ran))
-                f.close()
+    for key in algo:
+        generate_algo(key)
 
 
 if __name__ == '__main__': 
