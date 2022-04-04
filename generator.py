4616f00ae54800471c254261c96297aa40d957fe
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-04 21:00:50 +0100

    update generator to use tes_files_dir

--- generator.py
+++ generator.py
@@ -1,7 +1,9 @@
 import string
 import random
-import sys
+import os
 
+
+TEST_FILES_DIR = 'test_files'
 
 algo = {
     "aes" : [8, 64, 512, 4096, 32768, 262144, 2047152],
@@ -10,14 +12,20 @@ algo = {
 }
 
 
-def runCode(algo):
+def generate(algo):
+
+    if not os.path.isdir(TEST_FILES_DIR):
+        os.mkdir(TEST_FILES_DIR)
+
     for i in range(10):
         for (key, val) in algo.items():
             for size in val:
                 ran = "".join(random.choices(string.ascii_lowercase  + string.ascii_uppercase + string.digits, k=size))
-                f = open(key + str(i) + "_" + str(size), "w")
+                f = open( os.path.join(TEST_FILES_DIR, key + str(i) + "_" + str(size)) , "w" )
                 f.write(str(ran))
                 f.close()
 
-runCode(algo)
+
+if __name__ == '__main__': 
+    generate(algo)
 
